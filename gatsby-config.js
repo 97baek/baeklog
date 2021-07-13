bf5201702305661c11686a6d8594d89de34ff617
97baek <170410_@naver.com>
2021-07-14 00:13:42 +0900

    [UPDATE] 타입스크립트 적용

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,5 @@ module.exports = {
   siteMetadata: {
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-gatsby-cloud"],
+  plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-gatsby-cloud", "gatsby-plugin-typescript"],
 };
