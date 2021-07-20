3debdec3d8af50f65cddd668d0b038aa66b55961
97baek <170410_@naver.com>
2021-07-20 20:22:13 +0900

    [UPDATE] 작업표시줄에서 창 열기 기능 추가

--- src/components/Taskbar/Task/Task.tsx
+++ src/components/Taskbar/Task/Task.tsx
@@ -1,3 +1,4 @@
+import { useWindowContext } from '@hooks/useWindowContext';
 import React from 'react';
 import { ITask } from 'types/task';
 import { TaskIcon, TaskIcons } from './styles';
@@ -7,11 +8,15 @@ interface IProps {
 }
 
 const Task = ({ tasks }: IProps) => {
+  const [, , , openWindow] = useWindowContext();
+  // console.log(tasks);
   return (
     <>
       <TaskIcons>
         {tasks.map((task: ITask) => (
-          <TaskIcon key={task.id}>{task.name}</TaskIcon>
+          <TaskIcon key={task.id} onClick={() => openWindow(task.name)}>
+            {task.name}
+          </TaskIcon>
         ))}
       </TaskIcons>
     </>
