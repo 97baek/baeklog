8313356e1ce07d8bbc3a5cfd5b008075bd631c00
97baek <170410_@naver.com>
2021-07-24 00:11:24 +0900

    [FIX] 타입 이름 변경
    - ITaskInfo 이름 변경에 맞춰 변경했습니다.

--- src/components/Taskbar/Task/Task.tsx
+++ src/components/Taskbar/Task/Task.tsx
@@ -1,10 +1,10 @@
 import { useWindowContext } from '@hooks/useWindowContext';
 import React from 'react';
-import { ITask } from 'types/task';
+import { ITaskInfo } from '@typings/taskinfo';
 import { TaskIcon, TaskIcons, ActiveTask } from './styles';
 
 interface IProps {
-  tasks: ITask[];
+  tasks: ITaskInfo[];
 }
 
 const Task = ({ tasks }: IProps) => {
@@ -18,7 +18,7 @@ const Task = ({ tasks }: IProps) => {
   return (
     <>
       <TaskIcons>
-        {tasks.map((task: ITask) => (
+        {tasks.map((task: ITaskInfo) => (
           <TaskIcon key={task.id} onClick={() => onClickTask(task.name)}>
             {task.name} {task.isOpened ? <ActiveTask /> : ''}
           </TaskIcon>
