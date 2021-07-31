3520baab415fb4dc573d0d3fcf78bbc88d648027
97baek <170410_@naver.com>
2021-07-31 22:21:24 +0900

    [STYLE] Projects 창 내용 추가
    - Projects 컴포넌트의 내용을 채웠습니다.

--- src/components/Windows/Projects/styles.ts
+++ src/components/Windows/Projects/styles.ts
@@ -1,1 +1,19 @@
 import styled from 'styled-components';
+
+export const Container = styled.main`
+  display: flex;
+  background-color: white;
+  flex-direction: column;
+  width: 720px;
+  padding: 0 80px;
+`;
+
+export const ProjectWrap = styled.article``;
+
+export const ProjectTitle = styled.h2``;
+export const ProjectPeriod = styled.p``;
+export const ProjectPreview = styled.figure``;
+export const ProjectSkills = styled.p``;
+export const ProjectDesc = styled.p``;
+export const ProjectLinks = styled.div``;
+export const ProjectDetails = styled.details``;
